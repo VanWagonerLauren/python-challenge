06865fa9a2da06d4fd90dd798a513231cad38699
VanWagonerLauren <77031397+VanWagonerLauren@users.noreply.github.com>
2021-02-09 12:51:04 -0600

    Greatest Increase

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,7 +2,7 @@ import csv
 total_months = 0
 total_profit_loss_amount = 0.00
 average_profit_loss = 0.00
-greatest_increase = {}
+greatest_increase = {"date": "", "amount":0}
 greatest_decrease = {}
 
 file_path= "./resources/budget_data.csv"
@@ -17,16 +17,22 @@ with open(file_path) as csvfile:
     for row in csvreader:
         #The total number of months included in the dataset
         total_months = Total_months + 1
+        date = row[0]
+        profit = float(row[1])
 
-#The net total amount of "Profit/Losses" over the entire period
-#Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
-#The greatest increase in profits (date and amount) over the entire period  
-#The greatest decrease in losses (date and amount) over the entire period    
+        #The net total amount of "Profit/Losses" over the entire period
+        #Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+        #The greatest increase in profits (date and amount) over the entire period 
+         if (profit > greatest_increase ["amount"]):
+             greatest_increase["date"] = date
+             greatest_increase ["amount"] = profit
+        #The greatest decrease in losses (date and amount) over the entire period    
 
         #Print Results
         print ("financial analysis")
         print ("---------------------")
         print (f"Total Months: {total_months}")
+        print (f"Greatest Increase in Profits": {greatest_increase ['date']} (${greatest_increase ['amount']})")
 #Result should look like
 #Financial Analysis
 #----------------------------
