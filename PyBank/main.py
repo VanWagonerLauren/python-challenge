85d5ab01340e9479353536b69dcb4fd8c017e0c4
VanWagonerLauren <77031397+VanWagonerLauren@users.noreply.github.com>
2021-02-09 12:35:23 -0600

    reorganized layout of structure

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,18 +1,8 @@
 import csv
 total_months = 0
-
-
-#The net total amount of "Profit/Losses" over the entire period
 total_profit_loss_amount = 0.00
-
-
-#Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
 average_profit_loss = 0.00
-
-#The greatest increase in profits (date and amount) over the entire period
 greatest_increase = {}
-
-#The greatest decrease in losses (date and amount) over the entire period
 greatest_decrease = {}
 
 file_path= "./resources/budget_data.csv"
@@ -27,7 +17,11 @@ with open(file_path) as csvfile:
     for row in csvreader:
         #The total number of months included in the dataset
         total_months = Total_months + 1
-     
+
+#The net total amount of "Profit/Losses" over the entire period
+#Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+#The greatest increase in profits (date and amount) over the entire period  
+#The greatest decrease in losses (date and amount) over the entire period    
 
         #Print Results
         print ("financial analysis")
